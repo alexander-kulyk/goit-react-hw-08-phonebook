e7bb2c7862787d2f6e33f1acaa033248f29015b9
alexander-kulyk <kulyk.alexander@gmail.com>
2023-01-16 19:41:41 +0200

    style module

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,28 +1,21 @@
-import { useDispatch, useSelector } from "react-redux"
-import { handleFindContact } from "redux/contacts/filterContactsSlice";
-import { FilterIntput } from "./Filter.styled"
+import { useDispatch, useSelector } from 'react-redux';
+import { handleFindContact } from 'redux/contacts/filterContactsSlice';
+import css from './filter.module.css';
 
+export const Filter = () => {
+  const dispatch = useDispatch();
+  const query = useSelector(state => state.filter.filter);
 
-
-export const Filter = () =>{
-    
-    const dispatch = useDispatch();
-    const query = useSelector(state => state.filter.filter);
-
-    return(
-        <>
-            <FilterIntput
-                type="text"
-                name="filter"
-                placeholder="Find contacs by name"
-                value={query}
-                onChange = {e => dispatch(handleFindContact(e.target.value))}
-            />
-        </>
-
-    )
-}
-
-
-
-
+  return (
+    <>
+      <input
+        className={css.input}
+        type="text"
+        name="filter"
+        placeholder="Find contacs by name"
+        value={query}
+        onChange={e => dispatch(handleFindContact(e.target.value))}
+      />
+    </>
+  );
+};
